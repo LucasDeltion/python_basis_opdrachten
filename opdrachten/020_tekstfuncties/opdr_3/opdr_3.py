7f55d24950ac2ad93a1961523e91da7d41cd086b
PhutureGhost <97077204@st.deltion.nl>
2024-03-14 15:39:08 +0100

    030Opdr3

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,20 @@
 # Groep:
 
 # Hier komt je code...
+
+
+#Met onderstaande code kun je een mooie kerstboom op het scherm tonen
+#Aan jou de taak om vijf bomen naast elkaar te tekenen.
+
+#TIP: Maak gebruik van de mogelijkheid van python om met teksten een berekening uit te kunnen voeren
+
+
+print("    *      "*5)
+print("   ***     "*5)
+print("  ******   "*5)
+print(" ********  "*5)
+print("***********"*5)
+print("    ***    "*5)
+print("    ***    "*5)
+print("    ***    "*5)
+
