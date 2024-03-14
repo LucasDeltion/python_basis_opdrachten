b7da0ba4f496d95e481b9eb0a59cbeb4332d9cf7
PhutureGhost <97077204@st.deltion.nl>
2024-03-14 15:16:42 +0100

    020Opdr_2

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -4,6 +4,11 @@
 
 # Hier komt je code...
 
-my_list = ...
+#string defenitie
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
 
-print()
+#t counter
+t_count = (my_string).count('t')
+
+#de print
+print(f"De letter 't' komt {t_count} x voor in my_string")
