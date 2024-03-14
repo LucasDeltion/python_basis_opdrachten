93254d3bddd7e77c6fc9231c1ca44be47d2125e6
PhutureGhost <97077204@st.deltion.nl>
2024-03-14 16:03:06 +0100

    030Opdr_1

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,17 @@
 # Groep:
 
 # Hier komt je code...
+
+#vrienden + leeftijd
+Jurjen = 25
+Jordy = 26
+Edwin = 26
+
+#berekening
+totaal_leeftijd = Jurjen + Jordy + Edwin
+gemiddelde_leeftijd = totaal_leeftijd / 3
+
+#resultaat
+print("dit weten we over Jurjen, Jordy en Edwin:")
+print("De totale waarden van alle leeftijden bij elkaar opgeteld =", totaal_leeftijd)
+print("De gemiddelde leeftijd van deze boys is:", gemiddelde_leeftijd)
